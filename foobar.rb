bc9e82023cddaf843a153fba349d73efdd630b6a
Patricia Arenillo <parenillo@berkeley.edu>
2017-09-19 13:01:11 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,7 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map { |e| e.to_i + 2 }.select { |e| (e % 2 == 0 and e < 10) }.uniq.inject(0, &:+)
   end
 end
 
